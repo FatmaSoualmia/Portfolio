bb338a460f736574d0df17bcbe0d8adb745f007f
FatmaSoualmia <121440223+FatmaSoualmia@users.noreply.github.com>
2023-09-25 09:47:29 +0100

    Add files via upload

---
+++ src/data/SideBarData.js
@@ -0,0 +1,15 @@
+export const SideBarData = () => {
+  return (
+    [
+     {id:1, title:"HOME", link: "/"}, 
+     {id:2, title:"ABOUT", link: "about"}, 
+     {id:3, title:"RESUME", link: "resume"},
+     {id:4, title:"SERVICE", link: "service"},
+     {id:5, title:"PORTFOLIO", link: "portfolio"},
+     {id:6, title:"TESTIMONIALS", link: "testimonials"},
+     {id:7, title:"BLOG", link: "blog"},
+     {id:8, title:"CONTACT", link: "contact"},
+
+    ]
+  )
+}
